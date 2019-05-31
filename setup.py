cb2ec6b8dbe7e2d5d951fe7bdb70645d3d5b8a75
AJMAC1039 <ajmac1039@AJMAC1039.local>
2019-05-31 17:01:56 +0530

    initial push for pypi

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='libelastic',
+      version='0.1',
+      description='Elastic library for handelling elastic search mapping and data sync',
+      url='https://github.com/aasaanjobs/libelastic',
+      author='Rahul Kumar',
+      author_email='rahul.kumar@aasaanjobs.com',
+      license='MIT',
+      packages=['libelastic'],
+      zip_safe=False)
