1fc51209055bade046fd7e69b3439d47466ac011
shubhamgadia <shubham.gadia@aasaanjobs.com>
2019-06-17 10:34:47 +0530

    changed setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 
 setup(
     name='libelastic',
-    version='4.5',
+    version='4.6',
     packages=['elastic_mapper'],
     description='A line of description',
     long_description=README,
